c96cbf298b07df7d25aee8e447b396c9d104396c
marya <maryadukmak@gmail.com>
2020-02-07 23:14:54 +0100

    opdracht

--- 8.py
+++ 8.py
@@ -1,6 +1,14 @@
 def function():
- f= open("bestand.txt", "w")
- f.write("Hallo")
- f.close()
 
- function()
+ with open('bestand.txt', 'r') as i:
+    lines = i.readlines()
+
+ lines = [line.replace('/n', '') for line in lines]
+
+ with open('bestand.txt', 'w') as i:
+    i.writelines(lines)
+    i.close()
+
+
+function()
+
